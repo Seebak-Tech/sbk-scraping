cc0f4bab347598a5cfd0facd183c8eb64ba0e0e7
Emmanuel Martínez <emartinez@seebak.com.mx>
2022-01-05 15:23:17 -0600

    Fix bug tests_config

--- tests/config_test.py
+++ tests/config_test.py
@@ -1,6 +1,7 @@
 from sbk_scraping.config import AppConfig
 import pytest
 import environ
+import os
 
 
 def test_invalid_configuration():
@@ -12,8 +13,9 @@ def test_invalid_configuration():
 
 
 def test_config():
-    config = environ.to_config(
-        AppConfig,
-        environ={"APP_ROOTDIR": "/workspace/sbk-scraping"}
-    )
-    assert "/workspace/sbk-scraping/tests/test_data" == str(config.testdata)
+    config = environ.to_config(AppConfig)
+    env_var = os.environ.get(
+        "APP_ROOTDIR",
+        '/workspace/sbk-scraping/'
+    ) + "tests/test_data"
+    assert env_var == str(config.testdata)
