92ce5f78c05cb1b22072822420b1c1e56a073eb3
Emmanuel Martínez <emartinez@seebak.com.mx>
2022-01-05 15:35:54 -0600

    Fix testing of test_config function

--- tests/config_test.py
+++ tests/config_test.py
@@ -16,6 +16,7 @@ def test_config():
     config = environ.to_config(AppConfig)
     env_var = os.environ.get(
         "APP_ROOTDIR",
-        '/workspace/sbk-scraping/'
-    ) + "tests/test_data"
+        '/workspace/sbk-scraping'
+    ) + "/tests/test_data"
+
     assert env_var == str(config.testdata)
