97122b2c9e3bc6bf0f5419cfe3c006d3fd1607d0
Monica Nataly <nata.moonk@gmail.com>
2022-01-17 11:15:12 -0600

    Fix test and factories

--- sbk_scraping/parser/factories.py
+++ sbk_scraping/parser/factories.py
@@ -5,12 +5,7 @@ from sbk_scraping.parser.common import HttpResponseParser, NullParser
 from dataclasses import dataclass
 
 
-class InvalidParserType(Exception):
-    def __init__(self, msg):
-        Exception.__init__(self, msg)
-
-
-class InvalidParseId(Exception):
+class InvalidValue(Exception):
     def __init__(self, msg):
         Exception.__init__(self, msg)
 
@@ -36,54 +31,36 @@ class ParserFactory():
 
     def __get_config_parser(self) -> dict:
         jmes_expr = f"parsers[?parser_id=='{self.parser_id}'] | [0]"
-        cfg_parser = jmespath.search(
+        conf_parser = jmespath.search(
             expression=jmes_expr, data=load_parsers()
         )
-        if cfg_parser is None:
-            msg = '\n*Cause: The json file has invalid parser id'\
+        if conf_parser is None:
+            msg = '\n*Cause: The json file has invalid parser_id'\
                   '\n*Action: Validate the parse id'
-            raise InvalidParseId(msg)
-        return cfg_parser
+            raise InvalidValue(msg)
+        return conf_parser
 
-    def __parser_json(self, cfg_parser):
-        parser = self.__build_json_parser(cfg_parser['srch_expressions'])
-        return parser
+    def __parser_json(self, conf_parser):
+        return self.__build_json_parser(conf_parser['srch_expressions'])
 
-    def __parser_htmlxml(self, cfg_parser):
-        parser = self.__build_scrapy_parser(cfg_parser['srch_expressions'])
-        return parser
+    def __parser_htmlxml(self, conf_parser):
+        return self.__build_scrapy_parser(conf_parser['srch_expressions'])
 
-    def __build_parser(self) -> HttpResponseParser:
+    def __build_parser(self, conf_parser) -> HttpResponseParser:
         parser = NullParser()
-        cfg_parser = self.__get_config_parser()
-        parser_type = cfg_parser["parser_type"]
+        parser_type = conf_parser["parser_type"]
 
-        msg = '\n*Cause: The json file has invalid parser type'\
-              '\n*Action: Please validate the parser type'
-        
         if parser_type == "HtmlXml":
-            self.__parser_htmlxml(cfg_parser)
+            parser = self.__parser_htmlxml(conf_parser)
         elif parser_type == "Json":
-            self.__parser_json(cfg_parser)
+            parser = self.__parser_json(conf_parser)
         else:
-            raise InvalidParserType(msg)
+            msg = '\n*Cause: The json file has invalid parser_type'\
+                  '\n*Action: The valid values for parser_type are'\
+                  ' (HtmlXml, Json)'
+            raise InvalidValue(msg)
         return parser
 
     def build(self) -> HttpResponseParser:
-        return self.__build_parser()
-
-    #  def __build_parser(self) -> HttpResponseParser:
-        #  parser = NullParser()
-        #  cfg_parser = self.__get_config_parser()
-#
-        #  msg = '\n*Cause: The json file has invalid parser type'\
-              #  '\n*Action: Please validate the parser type'
-#
-        #  if cfg_parser["parser_type"] == "HtmlXml":
-            #  parser = self.__build_scrapy_parser(cfg_parser['srch_expressions'])
-        #  elif cfg_parser["parser_type"] == "Json":
-            #  parser = self.__build_json_parser(cfg_parser['srch_expressions'])
-        #  else:
-            #  raise InvalidParserType(msg)
-        #  return parser
-
+        conf_parser = self.__get_config_parser()
+        return self.__build_parser(conf_parser)
--- tests/parser/factories_test.py
+++ tests/parser/factories_test.py
@@ -1,30 +1,62 @@
 import pytest
-from sbk_scraping.parser.factories import (
-    ParserFactory,
-    InvalidParseId,
-    InvalidParserType
+from sbk_scraping.parser.factories import ParserFactory, InvalidValue
+from sbk_scraping.parser.scrapy_parser import HtmlXmlParser
+from sbk_scraping.parser.dynamic.json_parser import JsonParser
+
+
+htmlxml_parser_id = "First"
+json_parser_id = "Second"
+wrong_parser_type = "Third"
+wrong_parser_id = "wrong id"
+
+validations_to_try = [
+    (wrong_parser_id, r".*has invalid parser_id*"),
+    (wrong_parser_type, r".*has invalid parser_type*"),
+]
+
+validation_ids = [
+    "The parser_id is invalid",
+    "The parser_type is invalid",
+]
+
+
+@pytest.mark.parametrize(
+    'parser_id, match_msg',
+    validations_to_try,
+    ids=validation_ids
 )
-
-
-def test_parser_htmlxml_type(html_str):
-    parse = ParserFactory(data=html_str, parser_id="First")
-    result = parse.build()
-    print(result)
-
-
-def test_invalid_parser_id(html_str):
+def test_invalid_parser_values(html_str, parser_id, match_msg):
     with pytest.raises(
-        InvalidParseId,
-        match='has invalid parser id'
+        InvalidValue,
+        match=match_msg
     ):
-        parse = ParserFactory(data=html_str, parser_id="wrong id")
+        parse = ParserFactory(
+            data=html_str,
+            parser_id=parser_id,
+        )
         _ = parse.build()
 
 
-def test_invalid_parser_type(html_str):
-    with pytest.raises(
-        InvalidParserType,
-        match='has invalid parser type'
-    ):
-        parse = ParserFactory(data=html_str, parser_id="Third")
-        _ = parse.build()
+correct_validations_to_try = [
+    (htmlxml_parser_id, HtmlXmlParser),
+    (json_parser_id, JsonParser),
+]
+
+correct_validation_ids = [
+    "The parser is an instance of HtmlXmlParser",
+    "The parser_type is an instance of JsonParser",
+]
+
+
+@pytest.mark.parametrize(
+    'parser_id_valid, parser',
+    correct_validations_to_try,
+    ids=correct_validation_ids
+)
+def test_parser(html_str, parser_id_valid, parser):
+    parse = ParserFactory(
+        data=html_str,
+        parser_id=parser_id_valid,
+    )
+    instance = parse.build()
+    assert isinstance(instance, parser)
